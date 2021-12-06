9f00adea54f6468123e6eb11e7a8cd78b2903888
Gleb Sukristik <Gleb15a@gmail.com>
2021-12-06 13:37:13 +0300

    Добавить страницу описания книги

---
+++ presentation/Store.Web/Controllers/BookController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Store.Web.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly IBookRepository bookRepository;
+
+        public BookController(IBookRepository bookRepository)
+        {
+            this.bookRepository = bookRepository;
+        }
+
+        public IActionResult Index(int id)
+        {
+            Book book = bookRepository.GetById(id);
+
+            return View(book);
+        }
+    }
+}
