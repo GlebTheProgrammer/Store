3b17471ed34302b50397b47ac8949b86f27414f2
Gleb Sukristik <Gleb15a@gmail.com>
2021-12-15 15:39:48 +0300

    Реализовать оплату наличными

---
+++ domain/Store/OrderDelivery.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Реализация паттерна ValueObject - Подход, который не дайт изменять значения объекта после его создания
+namespace Store
+{
+    public class OrderDelivery
+    {
+        public string UniqueCode { get; }
+
+        public string Description { get; }
+
+        public decimal Amount { get; }  // Стоимость платной доставки
+
+        public IReadOnlyDictionary<string, string> Parameters { get; }
+
+        public OrderDelivery(string uniqueCode, string description, decimal amount,IReadOnlyDictionary<string,string> parameters)
+        {
+            if (string.IsNullOrWhiteSpace(uniqueCode))
+                throw new ArgumentException(nameof(uniqueCode));
+
+            if (string.IsNullOrWhiteSpace(description))
+                throw new ArgumentException(nameof(description));
+
+            if (parameters == null)
+                throw new ArgumentNullException(nameof(parameters));
+
+            UniqueCode = uniqueCode;
+            Description = description;
+            Amount = amount;
+            Parameters = parameters;
+        }
+    }
+}
--- presentation/Store.Web/Controllers/OrderController.cs
+++ presentation/Store.Web/Controllers/OrderController.cs
@@ -15,16 +15,19 @@ namespace Store.Web.Controllers
         private readonly IOrderRepository orderRepository;
         private readonly INotificationService notificationService;
         private readonly IEnumerable<IDeliveryService> deliveryServices;
+        private readonly IEnumerable<IPaymentService> paymentServices;
 
         public OrderController(IBookRepository bookRepository, 
                                IOrderRepository orderRepository,
                                INotificationService notificationService,
-                               IEnumerable<IDeliveryService> deliveryServices) // Constructor Injection
+                               IEnumerable<IDeliveryService> deliveryServices,
+                               IEnumerable<IPaymentService> paymentServices) // Constructor Injection
         {
             this.bookRepository = bookRepository;
             this.orderRepository = orderRepository;
             this.notificationService = notificationService;
             this.deliveryServices = deliveryServices;
+            this.paymentServices = paymentServices;
         }
 
         [HttpGet]
@@ -199,7 +202,9 @@ namespace Store.Web.Controllers
                             }); ;
             }
 
-            //todo: сохранить CellPhone
+            var order = orderRepository.GetById(id);
+            order.CellPhone = cellPhone;
+            orderRepository.Update(order);
 
             HttpContext.Session.Remove(cellPhone);
 
@@ -232,14 +237,55 @@ namespace Store.Web.Controllers
         {
             var deliveryService = deliveryServices.Single(service => service.UniqueCode == uniqueCode);
 
-            var form = deliveryService.MoveNext(id, step, values);
+            var form = deliveryService.MoveNextForm(id, step, values);
 
             if (form.IsFinal)
             {
-                return null;
+                var order = orderRepository.GetById(id);
+                order.Delivery = deliveryService.GetDelivery(form);
+                orderRepository.Update(order);
+
+                var model = new DeliveryModel
+                {
+                    OrderId = id,
+                    Methods = paymentServices.ToDictionary(service => service.UniqueCode,
+                                                                   service => service.Title),
+                };
+
+                return View("PaymentMethod",model);
             }
 
             return View("DeliveryStep", form);
+        }
+
+        [HttpPost]
+        public IActionResult StartPayment(int id, string uniqueCode)
+        {
+            var paymentService = paymentServices.Single(service => service.UniqueCode == uniqueCode);
+            var order = orderRepository.GetById(id);
+
+            var form = paymentService.CreateForm(order);
+
+            return View("PaymentStep", form);
+        }
+
+        [HttpPost]
+        public IActionResult NextPayment(int id, string uniqueCode, int step, Dictionary<string, string> values)
+        {
+            var paymentService = paymentServices.Single(service => service.UniqueCode == uniqueCode);
+
+            var form = paymentService.MoveNextForm(id, step, values);
+
+            if (form.IsFinal)
+            {
+                var order = orderRepository.GetById(id);
+                order.Payment = paymentService.GetPayment(form);
+                orderRepository.Update(order);
+
+                return View("Finish");
+            }
+
+            return View("PaymentStep", form);
         }
     }
 }
